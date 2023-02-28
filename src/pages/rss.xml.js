e1ddfafce7907bb28255c52b7cdd4a62056da241
Bernard Debecker <brnrd@proton.me>
2023-03-01 00:14:37 +0100

    feat: switch to full content in the RSS feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,18 @@
-import rss from '@astrojs/rss'
+import rss from '@astrojs/rss';
+import sanitizeHtml from 'sanitize-html';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../config'
 
-export const get = () =>
-	rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: import.meta.env.SITE,
-		items: import.meta.glob('./blog/**/*.{md,mdx}'),
-	})
+export function get(context) {
+  const postImportResult = import.meta.glob('./blog/*.md', { eager: true }); 
+  const posts = Object.values(postImportResult);
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: posts.map((post) => ({
+      link: post.url,
+      content: sanitizeHtml(post.compiledContent()),
+      ...post.frontmatter,
+    })),
+  });
+}
