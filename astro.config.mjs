cd71d48281e3a6a243668decd03961287b670e17
Bernard Debecker <bernard@vegetalope.com>
2026-09-25 22:20:30 +0200

    refactor: tightens design and animations (#15)

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,7 +17,7 @@ export default defineConfig({
 		},
 	},
 	build: {
-		inlineStylesheets: 'always',
+		inlineStylesheets: 'auto',
 	},
 	markdown: {
 		processor: satteri({ mdastPlugins: [readingTime()] }),
